b2017729d24b925f52523b3be238df954c109a6d
Helltar <mail@helltar.com>
2022-06-30 22:44:41 +0300

    update buttons size

--- src/forms/usettingsform.pas
+++ src/forms/usettingsform.pas
@@ -14,7 +14,7 @@ type
 
   TfrmSettings = class(TForm)
     btnSelectMountPoint: TButton;
-    btnpMain: TButtonPanel;
+    btnSave: TButton;
     cbAutorun: TCheckBox;
     edtMountPoint: TEdit;
     gbSettings: TGroupBox;
@@ -22,10 +22,10 @@ type
     lblMountPoint: TLabel;
     sddMountPoint: TSelectDirectoryDialog;
     seFontSize: TSpinEdit;
+    procedure btnSaveClick(Sender: TObject);
     procedure btnSelectMountPointClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormShow(Sender: TObject);
-    procedure OKButtonClick(Sender: TObject);
   end;
 
 var
@@ -46,6 +46,15 @@ begin
     edtMountPoint.Text := sddMountPoint.FileName;
 end;
 
+procedure TfrmSettings.btnSaveClick(Sender: TObject);
+begin
+  frmMain.config.mountPoint := edtMountPoint.Text;
+  frmMain.config.autorunState := cbAutorun.Checked;
+  frmMain.config.logFontSize := seFontSize.Value;
+  frmMain.synLog.Font.Size := seFontSize.Value;
+  Close;
+end;
+
 procedure TfrmSettings.FormCreate(Sender: TObject);
 begin
   cbAutorun.Checked := frmMain.config.autorunState;
@@ -57,15 +66,6 @@ procedure TfrmSettings.FormShow(Sender: TObject);
 begin
   Constraints.MinHeight := Height;
   Constraints.MinWidth := Width;
-end;
-
-procedure TfrmSettings.OKButtonClick(Sender: TObject);
-begin
-  frmMain.config.mountPoint := edtMountPoint.Text;
-  frmMain.config.autorunState := cbAutorun.Checked;
-  frmMain.config.logFontSize := seFontSize.Value;
-  frmMain.synLog.Font.Size := seFontSize.Value;
-  Close;
 end;
 
 end.
